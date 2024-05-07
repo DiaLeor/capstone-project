c5f656d16235b4b78b3d0a73b165f32f27e4af15
Dia Leor Paule <dia.paule@gmail.com>
2024-05-07 23:48:12 +0800

    Add time visualization.

--- MovieLens.R
+++ MovieLens.R
@@ -104,10 +104,60 @@ sum(is.na(edx)) # No NA values found
 paste(edx$userId, edx$movieId, sep = "-") %>%
   duplicated() %>% sum() # No duplicates found
 
-# I don't think the timestamp is relevant to what I'm trying to achieve.
-# So I'm going to remove it from the dataset.
-edx <- edx %>% select(userId,movieId,rating,title,genres)
-dim(edx)
-str(edx)
+# I'm not sure whether the timestamp is relevant to what I'm trying to achieve.
+# Convert timestamp to POSIXct
+edx$timestamp <- as.POSIXct(edx$timestamp, origin = "1970-01-01", tz = "UTC")
 
+# Aggregate data by year
+ratings_yearly <- edx %>%
+  mutate(year = format(timestamp, "%Y")) %>%
+  group_by(year) %>%
+  summarise(total_ratings = n())
 
+# Plot yearly ratings
+ggplot(ratings_yearly, aes(x = year, y = total_ratings)) +
+  geom_point() +
+  scale_x_discrete(name = "Year") +
+  scale_y_continuous(name = "Total Ratings")
+
+# A good research question:
+  # is an actual question, not a statement.
+  # is open-ended
+  # grounded in theory, prior research, or practice
+  # not too simple, not too complex
+
+# Prasing a question prevents you from being biased toward what you expect to
+# see after you analyze the data and set some more neutral tone that allows your
+# skillful analysis of the data to reveal evidence of an answer. In general,
+# questions that begin with "why" are not good research questions because they
+# tend to be too complex and opinion based.
+
+# How do user preferences vary across different genres?
+
+# We start by posing the question we're trying to answer in terms of the
+# subject matter, not using data or statistical jargon. From there, we come
+# up with a suite of simpler, more closed ended data questions that we can
+# answer through exploring, visualizing, and analyzing the data.
+
+# Question board: we start at the top with our open-ended, grounded, not-too-
+# simple-not-too-complex research question. We can then break this question
+# down into a series of more specific, closed-ended questions. Some of these
+# questions, we may not be able to answer if there are limitations to the data
+# available to us. Other data questions may lead to additional data questions
+# or more specific research questions that could be addressed among a smaller
+# team or at a different time. If you find that the process of translating a
+# research question into data questions is too hard, it's important to take a
+# step back and evaluate if your research question is good enough or it needs
+# to be refined:
+  
+# Rubric:
+  # Does the question strike the right balance between simple and complex?
+  # Will it take more than a simple data analysis or a single visualization
+  # to answer the question?
+  # Do we have access to the right amount and the right types of data to
+  # sufficiently answer the question?
+  # Will it be clear when we've sufficiently answered the question?
+
+# Thinking through your research question helps to storyboard the process to
+# collecting the right data, performing the right analysis, creating the right
+# visualizations, and telling the right story.
